0f115425b53d3369819aea52fd9122fcee95f57c
Mudassir <mudassir@Mudassirs-MacBook-Pro.local>
2014-07-10 00:37:40 +0530

    added gravatar & included work in progress alert

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 var fs = require('fs');
 var gravatar = require('gravatar');
 var schema = require('resume-schema');
-var resume = schema.resumeJson;
+var resumeObject = schema.resumeJson;
 var Mustache = require('mustache');
 
+function hasPersonalEmail(resumeObject) {
+	return (resumeObject.bio && resumeObject.bio.email && resumeObject.bio.email.personal);
+}
+
 function render () {
+	if (hasPersonalEmail(resumeObject)) {
+		resumeObject.bio.gravatar = gravatar.url(resumeObject.bio.email.personal, {
+			s: '100',
+			r: 'pg',
+			d: 'mm'
+		});
+	}
+
 	var theme = fs.readFileSync(__dirname + '/resume.template', 'utf8');
-	var resumeHTML = Mustache.render(theme, resume);
+	var resumeHTML = Mustache.render(theme, resumeObject);
+	
 	return resumeHTML;
 }
 
