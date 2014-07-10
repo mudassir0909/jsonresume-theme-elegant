292045b54c00a5b07b4dd1b1fde502d11df2b909
mudassir0909 <mudassir.dev@gmail.com>
2014-07-10 07:29:52 +0530

    Added work experience

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 var fs = require('fs');
 var gravatar = require('gravatar');
-var schema = require('resume-schema');
-var resumeObject = schema.resumeJson;
 var Mustache = require('mustache');
+var _ = require('underscore');
 
 function hasPersonalEmail(resumeObject) {
 	return (resumeObject.bio && resumeObject.bio.email && resumeObject.bio.email.personal);
 }
 
-function render () {
+function render (resumeObject) {
 	if (hasPersonalEmail(resumeObject)) {
 		resumeObject.bio.gravatar = gravatar.url(resumeObject.bio.email.personal, {
 			s: '100',
@@ -19,6 +18,12 @@ function render () {
 
 	var theme = fs.readFileSync(__dirname + '/resume.template', 'utf8');
 	var resumeHTML = Mustache.render(theme, resumeObject);
+	
+	_.each(resumeObject.work, function (work_experience) {
+		if (! work_experience.endDate) {
+			work_experience.endDate = 'Present';
+		}
+	});
 	
 	return resumeHTML;
 }
