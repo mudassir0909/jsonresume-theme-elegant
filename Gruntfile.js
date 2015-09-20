3a7846b72f5064cb5bd596e460d29da0304b3aff
mudassir0909 <mudassir.dev@gmail.com>
2015-09-21 00:31:58 +0530

    Added less to grunt build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -80,6 +80,7 @@ module.exports = function(grunt) {
         // 'copy:resumejson',
         'clean',
         'copy:build',
+        'less',
         'exec:build_index' //,
         /* Uncomment this item (and the comma above) if you add a favicon.ico
            in the project root. You'll also need to uncomment the <link...> tag
