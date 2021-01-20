8ab437cb767ad5d70e8457ad57441714ebe7f78e
Mudassir Ali <mudassir.dev@gmail.com>
2021-01-21 00:20:42 +1100

    remove legacy code

--- index.js
+++ index.js
@@ -1,168 +1,45 @@
+const ReactDOMServer = require('react-dom/server');
+const React = require('react');
+const { Foo } = require('./theme');
 const fs = require('fs');
+const path = require('path');
 
-const _ = require('underscore');
-const utils = require('jsonresume-themeutils');
-const moment = require('moment');
-const markdown = require('markdown-it')({
-    breaks: true
-}).use(require('markdown-it-abbr'));
+function render() {
+  const reactElementString = ReactDOMServer.renderToString(
+    React.createElement(Foo),
+  );
+  const componentjs = fs.readFileSync(path.join(__dirname, './theme.js')).toString();
+  return buildHtml(reactElementString, 'Foo', componentjs);
+}
 
-const { renderResume } = require('./tpl/index');
+function buildHtml(reactElementString, reactElementTag, componentjs) {
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-require('./moment-precise-range.js');
-
-utils.setConfig({ date_format: 'MMM, YYYY' });
-
-function render(resume) {
-    const addressAttrs = ['address', 'city', 'region', 'countryCode', 'postalCode'];
-    const addressValues = addressAttrs.map(key => resume.basics.location[key]);
-    const css = fs.readFileSync(__dirname + '/assets/css/theme.css', 'utf-8');
-
-    resume.basics.picture = utils.getUrlForPicture(resume);
-    resume.basics.summary = convertMarkdown(resume.basics.summary);
-    resume.basics.computed_location = _.compact(addressValues).join(', ');
-
-    if (resume.languages) {
-        resume.basics.languages = _.pluck(resume.languages, 'language').join(', ');
-    }
-
-    _(resume.basics.profiles).forEach(p => {
-        const label = p.network.toLowerCase();
-
-        p.url = utils.getUrlForProfile(resume, label);
-        p.label = label;
-    });
-
-    resume.basics.top_five_profiles = resume.basics.profiles.slice(0, 5);
-    resume.basics.remaining_profiles = resume.basics.profiles.slice(5);
-
-    _(resume.work).forEach(work_info => {
-        const start_date = moment(work_info.startDate, 'YYYY-MM-DD');
-        const end_date = moment(work_info.endDate, 'YYYY-MM-DD');
-        const can_calculate_period = start_date.isValid() && end_date.isValid();
-
-        if (can_calculate_period) {
-            work_info.duration = moment.preciseDiff(start_date, end_date);
-        }
-
-        if (start_date.isValid()) {
-          work_info.startDate = utils.getFormattedDate(start_date);
-        }
-
-        if (end_date.isValid()) {
-          work_info.endDate = utils.getFormattedDate(end_date);
-        }
-
-        work_info.summary = convertMarkdown(work_info.summary);
-
-        work_info.highlights = _(work_info.highlights)
-            .map(highlight => convertMarkdown(highlight));
-    });
-
-    _(resume.skills).forEach(skill_info => {
-        const levels = ['Beginner', 'Intermediate', 'Advanced', 'Master'];
-
-        if (skill_info.level) {
-            skill_info.skill_class = skill_info.level.toLowerCase();
-            skill_info.level = capitalize(skill_info.level.trim());
-            skill_info.display_progress_bar = _.contains(levels, skill_info.level);
-        }
-    });
-
-    _(resume.education).forEach(education_info => {
-        ['startDate', 'endDate'].forEach(type => {
-            const date = education_info[type];
-
-            if (date) {
-                education_info[type] = utils.getFormattedDate(date);
-            }
+      <title>React SSR</title>
+    </head>
+    <body>
+      <div id="root">${reactElementString}</div>
+      <script crossorigin src="https://unpkg.com/react@16/umd/react.development.js"></script>
+      <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
+      <script>
+       ${componentjs}
+      </script>
+      <script>
+        window.addEventListener("DOMContentLoaded", function() {
+          ReactDOM.hydrate(
+            React.createElement(${ reactElementTag }),
+            document.getElementById('root')
+          );
         });
-    });
-
-    _(resume.awards).forEach(a => {
-        const date = a.date;
-
-        a.summary = convertMarkdown(a.summary);
-
-        if (date) {
-            a.date = utils.getFormattedDate(date, 'MMM DD, YYYY');
-        }
-    });
-
-    _(resume.volunteer).forEach(v => {
-        v.summary = convertMarkdown(v.summary);
-
-        ['startDate', 'endDate'].forEach(type => {
-            const date = v[type];
-
-            if (date) {
-                v[type] = utils.getFormattedDate(date);
-            }
-        });
-
-        v.highlights = _(v.highlights).map(convertMarkdown);
-    });
-
-    _(resume.publications).forEach(p => {
-        const date = p.releaseDate;
-
-        p.summary = convertMarkdown(p.summary);
-
-        if (date) {
-            p.releaseDate = utils.getFormattedDate(date, 'MMM DD, YYYY');
-        }
-    });
-
-    _(resume.references).forEach(r => {
-        r.reference = convertMarkdown(r.reference);
-    });
-
-    return renderResume({
-        resume: resume,
-        floating_nav_items: getFloatingNavItems(resume),
-        css: css,
-        _: _
-    });
-}
-
-function interpolate(object, keyPath) {
-    const keys = keyPath.split('.');
-
-    return keys.reduce((res, key) => (res || {})[key], object);
-}
-
-function capitalize(str) {
-    if (str) {
-        str = str.toString();
-        return str[0].toUpperCase() + str.slice(1).toLowerCase();
-    }
-
-    return str;
-}
-
-function convertMarkdown(str) {
-    if (str != null) {
-        return markdown.render(str);
-    }
-}
-
-function getFloatingNavItems(resume) {
-    const floating_nav_items = [
-        {label: 'About', target: 'about', icon: 'board', requires: 'basics.summary'},
-        {label: 'Work Experience', target: 'work-experience', icon: 'office', requires: 'work'},
-        {label: 'Skills', target: 'skills', icon: 'tools', requires: 'skills'},
-        {label: 'Education', target: 'education', icon: 'graduation-cap', requires: 'education'},
-        {label: 'Awards', target: 'awards', icon: 'trophy', requires: 'awards'},
-        {label: 'Volunteer Work', target: 'volunteer-work', icon: 'child', requires: 'volunteer'},
-        {label: 'Publications', target: 'publications', icon: 'newspaper', requires: 'publications'},
-        {label: 'Interests', target: 'interests', icon: 'heart', requires: 'interests'},
-        {label: 'References', target: 'references', icon: 'thumbs-up', requires: 'references'}
-    ];
-
-    return _(floating_nav_items).filter(item => {
-        const value = interpolate(resume, item.requires);
-        return !_.isEmpty(value);
-    });
+      </script>
+    </body>
+  </html>
+  `;
 }
 
 module.exports = { render };
--- serve.js
+++ serve.js
@@ -1,54 +1,14 @@
-//
-// This script will run a local development server. This is useful when
-// developing the theme.
-//
-// Usage:
-// `node serve`
-//
-
 const http = require('http');
-const fs = require('fs');
-const resume = JSON.parse(fs.readFileSync('node_modules/resume-schema/resume.json', 'utf8'));
 const theme = require('./index.js');
-const path = require('path');
 
 const PORT = 8888;
 
-http.createServer(function(req, res) {
-    const picture = resume.basics.picture && resume.basics.picture.replace(/^\//, '');
-
-    if (picture && req.url.replace(/^\//, '') === picture.replace(/^.\//, '')) {
-        const format = path.extname(picture);
-        try {
-            const image = fs.readFileSync(picture);
-            res.writeHead(200, {
-                'Content-Type': `image/${format}`,
-            });
-            res.end(image, 'binary');
-        } catch (error) {
-            if (error.code === 'ENOENT') {
-                console.log('Picture not found !');
-                res.end();
-            } else {
-                throw error;
-            }
-        }
-    } else {
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
-        res.end(render());
-    }
+http.createServer(function (req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+  res.end(theme.render());
 }).listen(PORT);
 
 console.log(`Preview: http://localhost:${PORT}/`);
 console.log('Serving..');
-
-function render() {
-    try {
-        return theme.render(JSON.parse(JSON.stringify(resume)));
-    } catch (e) {
-        console.log(e.message);
-        return '';
-    }
-}
